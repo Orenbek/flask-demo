962606b65d8a59466e7dbf895eac0858368ade03
wurenbieke <wurenbieke@bytedance.com>
2022-11-01 11:09:01 +0800

    feat: 支持文件上传

--- app.py
+++ app.py
@@ -1,56 +1,28 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request, url_for
+import os
 
 app = Flask(__name__)
-
 url_for
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    for key, value in request.files.items():
+        os.makedirs(f'./static/imgs/{os.path.dirname(key)}', exist_ok=True)
+        value.save(f'./static/imgs/{key}')
+    return jsonify({'code': 0})
 
-@app.route('/hello/<name>')
-def hello(name):
-    return f'''
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>页面标题</title>
-    </head>
-    <body>
-    
-    <content class="container">
-    <div class="row">
-        <div class="col-md-12">
-        <h1>页面标题</h1>
-        <p>你好：{name}</p>
-        </div>
-    </div>
-    </content>
-    </body>
-</html>
-'''
-
-
-@app.route('/book/<id>', methods=['POST'])
-def books(id):
-    # print(request.method)       # 请求方法
-    # print(request.headers)      # 请求的headers
-    # print(request.path)         # 资源路径
-    # print(request.url)          # 完整的url
-    # print(request.remote_addr)  # 客户端IP
-    # print(request.cookies)      # 请求的cookie
-    # print(request.args)         # 请求的参数
-    print(request.form)         # 请求的表单
-    # pic = request.files['picture']
-    # pic.save(f'./static/{pic.filename}')
-    # res = process_img(pic)
-    # print(request.files['picture'])
-    return id
+@app.route('/test', methods=['POST'])
+def test():
+    img = request.files['img']
+    print(img)
+    return jsonify({'code': 0, 'data': {
+        'name': img.filename,
+        'size': img.content_length,
+    }})
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -9,11 +9,48 @@ function addEvent(element, event, callback) {
   element.addEventListener(event, callback);
 }
 
-const button1 = getElementById('button1');
-const button2 = getElementById('button2');
-const input1 = getElementById('input1');
-const input2 = getElementById('input2');
+const button1 = getElementById("button1");
+const button2 = getElementById("button2");
+const input1 = getElementById("input1");
+const input2 = getElementById("input2");
+const imgInfo = getElementById("imginfo");
 
-addEvent(button1, 'click', () => {});
-addEvent(button2, 'click', () => {});
-console.log(button1, button2, input1, input2);
+addEvent(button1, "click", () => {
+  const files = [...input1.files];
+  const imgFiles = files.filter((file) => file.type.includes("image"));
+  if (imgFiles.length === 0) {
+    alert("没有图片文件");
+    return;
+  }
+  const formData = new FormData();
+  imgFiles.forEach((img) => formData.append(img.webkitRelativePath, img));
+  fetch("/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log({ res });
+      if (res.code === 0) {
+        alert("上传成功");
+      }
+    });
+});
+addEvent(button2, "click", () => {
+  if (input2.files.length === 0) {
+    alert("没有图片文件");
+    return;
+  }
+  const formData = new FormData();
+  formData.append("img", input2.files[0]);
+  fetch("/test", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.code === 0) {
+        imgInfo.innerText = JSON.stringify(res.data);
+      }
+    });
+});
